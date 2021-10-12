579a96218f4ab02039f55a14945146b8ea803105
Samuel Gamboa <Samuel.Gamboa.Tuz@gmail.com>
2021-10-12 12:11:33 -0500

    Add sampleMetadata object

---
+++ R/sampleMetadata.R
@@ -0,0 +1,7 @@
+#' sampleMetadata
+#'
+#' A data frame with the combined metadata of all of the samples in the
+#' datasets provided through the MicrobiomeBenchmarkData.
+#'
+#' @format A data.frame.
+"sampleMetadata"
