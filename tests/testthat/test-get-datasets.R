58d896938f45c19e07a38b220568d710b64060d3
Samuel Gamboa <Samuel.Gamboa.Tuz@gmail.com>
2021-10-12 12:10:55 -0500

    Update tests for the getDataset function

--- tests/testthat/test-get-datasets.R
+++ tests/testthat/test-get-datasets.R
@@ -1,19 +1,10 @@
-test_that("getDatasets works.", {
+test_that("getDataset works.", {
 
-  datasets <- getDatasets()
+  dataset_names <- getDataset()
+  expect_true(is.character(dataset_names))
+  expect_message(getDataset(), "Use vignette")
 
-  vector_ok <- names(getDatasets(datasets[1:2]))
-
-  vector_not_chr <- 1:3
-  vector_duplicated <- c("calgaro_2020_16S_gingival_healthy", "calgaro_2020_16S_gingival_healthy", "calgaro_2020_16S_gingival_healthy")
-  vector_na <- c("calgaro_2020_16S_gingival_healthy", NA, NA)
-  vector_not <- c("calgaro_2020_16S_gingival_healthy", "calgaro_2020", "calgaro_2019")
-
-  output <- getDatasets(vector_ok)
-  expect_s4_class(output[[1]], "SummarizedExperiment")
-
-  expect_error(getDatasets(vector_not_chr), regexp = "character")
-  expect_error(getDatasets(vector_duplicated), regexp = "duplicate")
-  expect_error(getDatasets(vector_na), regexp = "NA")
+  tse <- getDataset("HMP_2012_16S_gingival_V35_subset", dryrun = FALSE)[[1]]
+  expect_s4_class(tse, "TreeSummarizedExperiment")
 
 })
