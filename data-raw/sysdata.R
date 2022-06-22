9470b77c06c9ab537c2ec7cdde61c7441b79819b
Samuel D. Gamboa-Tuz <53412049+sdgamboa@users.noreply.github.com>
2022-06-22 08:13:16 -0400

    Update sysdata.R

--- data-raw/sysdata.R
+++ data-raw/sysdata.R
@@ -4,10 +4,8 @@ metadata_csv <- system.file(
     package = "MicrobiomeBenchmarkData"
 )
 metadata <- utils::read.csv(metadata_csv)
-
 titles <- sub("_[a-z]+_[a-z]+$", "", metadata$Title)
 titles <- unique(titles[titles != "sampleMetadata"])
-
 usethis::use_data(
     metadata, titles,
     internal = TRUE, overwrite = TRUE
