7281d4044d90b2f3bad9d4ce27163583065b8358
sdgamboa <samuel.gamboa.tuz@gmail.com>
2022-06-21 23:51:26 -0400

    Add new links (temporary on github). And update sampleMetadata.

---
+++ data-raw/sampleMetadata.R
@@ -0,0 +1,9 @@
+
+fname <- system.file(
+    'extdata/metadata.csv', package = 'MicrobiomeBenchmarkData'
+)
+
+metadata_csv <- utils::read.csv(fname)
+url <- metadata_csv[metadata_csv$Title == 'sampleMetadata',]$SourceUrl
+sampleMetadata <- readr::read_tsv(url)
+usethis::use_data(sampleMetadata, overwrite = TRUE)
