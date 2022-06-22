48c47f47c50e9146a6af57366d4c633aa62ccdd4
Samuel D. Gamboa-Tuz <53412049+sdgamboa@users.noreply.github.com>
2022-06-22 08:12:58 -0400

    Update sampleMetadata.R

--- data-raw/sampleMetadata.R
+++ data-raw/sampleMetadata.R
@@ -2,7 +2,6 @@
 fname <- system.file(
     'extdata/metadata.csv', package = 'MicrobiomeBenchmarkData'
 )
-
 metadata_csv <- utils::read.csv(fname)
 url <- metadata_csv[metadata_csv$Title == 'sampleMetadata',]$SourceUrl
 sampleMetadata <- readr::read_tsv(url)
